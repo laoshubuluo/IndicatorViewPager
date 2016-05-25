9656df2566eac162f80e95324e757b0342d9cd4d
laoshubuluo <541276229@qq.com>
2016-05-25 17:50:58 +0800

    增加GuideActivity

--- app/src/main/java/rat/com/indicator/GuideActivity.java
+++ app/src/main/java/rat/com/indicator/GuideActivity.java
@@ -2,16 +2,23 @@ package rat.com.indicator;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.shizhefei.view.indicator.FixedIndicatorView;
+import com.shizhefei.view.indicator.IndicatorViewPager;
 
 /**
  * Created by liangjinzhu on 16/5/24.
  */
-public class GuideActivity extends FragmentActivity implements View.OnClickListener {
-//    private Button guideBtn;
-//    private Button tabMainBtn;
-//    private Button slideTabMainBtn;
-//    private Button springTabBtn;
+public class GuideActivity extends FragmentActivity {
+    private IndicatorViewPager indicatorViewPager;
+    private LayoutInflater inflater;
+    private ViewPager viewPager;
+    private FixedIndicatorView indicatorView;
+    private int[] images = {R.mipmap.guid1, R.mipmap.guid2, R.mipmap.guid3, R.mipmap.guid4, R.mipmap.guid5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +28,36 @@ public class GuideActivity extends FragmentActivity implements View.OnClickListe
     }
 
     private void initView() {
-//        guideBtn = (Button) findViewById(R.id.guideBtn);
-//        tabMainBtn = (Button) findViewById(R.id.tabMainBtn);
-//        slideTabMainBtn = (Button) findViewById(R.id.slideTabMainBtn);
-//        springTabBtn = (Button) findViewById(R.id.springTabBtn);
-//        guideBtn.setOnClickListener(this);
-//        tabMainBtn.setOnClickListener(this);
-//        slideTabMainBtn.setOnClickListener(this);
-//        springTabBtn.setOnClickListener(this);
+        inflater = LayoutInflater.from(getApplicationContext());
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        indicatorView = (FixedIndicatorView) findViewById(R.id.indicatorView);
+        indicatorViewPager = new IndicatorViewPager(indicatorView, viewPager);
+        indicatorViewPager.setAdapter(adapter);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.guideBtn:
-                break;
-            case R.id.tabMainBtn:
-                break;
-            case R.id.slideTabMainBtn:
-                break;
-            case R.id.springTabBtn:
-                break;
+    private IndicatorViewPager.IndicatorPagerAdapter adapter = new IndicatorViewPager.IndicatorViewPagerAdapter() {
+        @Override
+        public int getCount() {
+            return images.length;
         }
-    }
+
+        @Override
+        public View getViewForTab(int position, View convertView, ViewGroup viewGroup) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.tab_guide, viewGroup, false);
+            }
+            return convertView;
+        }
+
+        @Override
+        public View getViewForPage(int position, View convertView, ViewGroup viewGroup) {
+            if (convertView == null) {
+                convertView = new View(getApplicationContext());
+                convertView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            }
+            convertView.setBackgroundResource(images[position]);
+            return convertView;
+        }
+    };
 }
 
